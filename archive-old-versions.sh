d62305cb95a4bcfc4cdb1cff2ed98e9edf911500
Pierre Besson <pbesson@ippon.fr>
2016-07-12 17:20:11 +0200

    fix the script to use the documentation-archive/ url path

--- archive-old-versions.sh
+++ archive-old-versions.sh
@@ -1,17 +1,16 @@
-mkdir public
 git clone https://github.com/jhipster/jhipster.github.io.git
 cd jhipster.github.io
 for tag in $(git tag)
 do
-    mkdir ../public/$tag
-    chmod 777 ../public/$tag
+    mkdir ../$tag
+    chmod 777 ../$tag
 
 	  echo "Building docs for $tag"
     rm Gemfile
     git checkout $tag
 cat >  _config-baseurl.yml << EOF
-baseurl: /$tag
-url: /$tag
+baseurl: /documentation-archive/$tag
+url: /documentation-archive/$tag
 EOF
 
 if [ ! -f Gemfile ]
@@ -19,5 +18,5 @@ then
     echo "gem 'github-pages'">Gemfile
 fi
     bundle install
-    bundle exec jekyll build -d ../public/$tag --config _config.yml,_config-baseurl.yml
+    bundle exec jekyll build -d ../$tag --config _config.yml,_config-baseurl.yml
 done
