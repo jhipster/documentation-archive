12e94fa0228e459c830b9f104f98f76d05388fd2
Julien Dubois <julien.dubois@gmail.com>
2017-10-28 00:14:33 +0200

    Release v4.10.2

--- archive-old-versions.sh
+++ archive-old-versions.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/jhipster/jhipster.github.io.git
+git clone https://github.com/jhipster/jhipster.github.io.git --depth=1
 cd jhipster.github.io
 for tag in $(git tag)
 do
